57f53c8698586756aa161609eb44535ce7ac6045
artahian <artahian@gmail.com>
2025-12-12 15:00:54 -0800

    Add mutation and user context examples

--- src/server/example/db.ts
+++ src/server/example/db.ts
@@ -4,6 +4,7 @@ export const dbExampleItems = new Store('exampleItems', {
   schema: {
     title: schema.string(),
     createdAt: schema.date(),
+    userId: schema.userId(),
   },
   indexes: []
 });
