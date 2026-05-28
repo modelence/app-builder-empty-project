e8e72a75c1ce1eb03cc9f4554a2050ac63075dfc
omegascorp <omegascorp@gmail.com>
2026-05-28 23:02:38 +0400

    migrate config to js

---
+++ mobile/app.config.js
@@ -0,0 +1,26 @@
+const modelenceBaseUrl =
+  process.env.MODELENCE_SITE_URL ?? 'https://localhost:3000';
+
+module.exports = {
+  expo: {
+    name: 'Modelence Mobile',
+    slug: 'modelence-mobile',
+    version: '0.1.0',
+    orientation: 'portrait',
+    userInterfaceStyle: 'light',
+    ios: {
+      supportsTablet: true,
+    },
+    android: {
+      adaptiveIcon: {
+        backgroundColor: '#ffffff',
+      },
+    },
+    web: {
+      bundler: 'metro',
+    },
+    extra: {
+      modelenceBaseUrl,
+    },
+  },
+};
