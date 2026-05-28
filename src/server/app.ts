ae299dcf19ce5a15a2a1e06fc24ac41985e87eb7
omegascorp <omegascorp@gmail.com>
2026-05-28 18:14:51 +0400

    add expo direct

--- src/server/app.ts
+++ src/server/app.ts
@@ -6,7 +6,7 @@ startApp({
   modules: [exampleModule /* Add your modules here */],
 
   security: {
-    frameAncestors: ['https://modelence.com', 'https://*.modelence.com', 'http://localhost:*'],
+    frameAncestors: ['https://modelence.com', 'https://*.modelence.com', 'http://localhost:*', 'https://*.exp.direct'],
   },
 
   migrations: [{
